83bbcceea33e61638ce18ca8bd8822f0f0c5ceba
Rusimka Dineva <rusimka.dineva@scalefocus.com>
2023-05-09 13:38:11 +0200

    Exercises

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import interfaces.Calculator;
 import interfaces.impl.CalculatorImpl;
 import interfaces.impl.SayInterfaceImpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -14,7 +22,6 @@ public class Main {
     ima vo AbstractInterface1, go ima i vo SayInterface
      */
 
-
     Calculator add = ((x, y) -> x + y);
     Calculator subtract = ((x, y) -> x - y);
     int sum = add.calculate(5, 3);
@@ -22,5 +29,33 @@ public class Main {
 
     System.out.println("Sum: " + sum);
     System.out.println("Difference: " + difference);
+
+    /*
+    Exercise 1: For given stream of integer values, take only the even numbers, multiple by 10, and the result should be in array
+     */
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+    Predicate<Integer> isEven = number -> number % 2 == 0;
+    Function<Integer, Integer> multipleByTen = number -> number * 10;
+    List<Integer> resultList =
+        numbers.stream().filter(isEven).map(multipleByTen).collect(Collectors.toList());
+    System.out.println("Result: " + resultList);
+
+    /*
+    Exercise 2: Function for given stream of numbers to return the smallest one
+     */
+    Function<List<Integer>, Integer> findSmallestNumber =
+        list -> list.stream().min(Integer::compareTo).orElse(null);
+    int smallestNumber = findSmallestNumber.apply(numbers);
+    System.out.println(smallestNumber);
+
+    /*
+    Exercise 3: Function that will convert array list in stream and for each element if it's divisible by three, to print out that's divisible with three
+     */
+    ArrayList<Integer> numbers2 =
+        new ArrayList<>(Arrays.asList(1, 3, 4, 9, 12, 15, 16, 18, 21, 25, 27, 30));
+    Predicate<Integer> isDivisibleByThree = number -> number % 3 == 0;
+    Consumer<ArrayList<Integer>> displayDivisibleNumbers =
+        list -> list.stream().filter(isDivisibleByThree).forEach(System.out::println);
+    displayDivisibleNumbers.accept(numbers2);
   }
 }
